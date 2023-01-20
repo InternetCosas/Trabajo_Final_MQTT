b833532a4570e323121f8fb5473b6dde9d1af5b9
María Naranjo Almeida <41172222+marianaral@users.noreply.github.com>
2023-01-20 14:02:39 +0000

    ci: 🚧 Añadida parte de comunicación con LoRa

--- Trabajo_Final/sensores/termistor.ino
+++ Trabajo_Final/sensores/termistor.ino
@@ -4,6 +4,41 @@
  */
 
 #include <math.h>
+#include <string.h>
+#include <SPI.h>             
+#include <LoRa.h>
+#include <Arduino_PMIC.h>
+
+#define TX_LAPSE_MS          10000
+
+// NOTA: Ajustar estas variables 
+const uint8_t localAddress = 0xB3;     // Dirección de este dispositivo
+uint8_t destination = 0xB0;            // Dirección de destino, 0xFF es la dirección de broadcast
+
+volatile bool txDoneFlag = true;       // Flag para indicar cuando ha finalizado una transmisión
+volatile bool transmitting = false;
+
+// Estructura para almacenar la configuración de la radio
+typedef struct {
+  uint8_t bandwidth_index;
+  uint8_t spreadingFactor;
+  uint8_t codingRate;
+  uint8_t txPower; 
+} LoRaConfig_t;
+
+double bandwidth_kHz[10] = {7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3,
+                            41.7E3, 62.5E3, 125E3, 250E3, 500E3 };
+
+LoRaConfig_t thisNodeConf   = { 9, 7, 5, 2};
+int remoteRSSI = 0;
+float remoteSNR = 0;
+static uint16_t msgCount = 0;
+
+const int LDR_Apin = A1;
+const int LDR_Dpin = 2;
+int wait = 10000;
+uint8_t d_read = -1;
+uint16_t a_read = -1;
 
 const int Rc = 21000; //valor de la resistencia
 const int Vcc = 5;
@@ -17,12 +52,88 @@ float K = 2.5; //factor de disipacion en mW/C
 
 void setup()
 {
-  Serial.begin(9600);
+  pinMode(LDR_Apin, INPUT); // Pin por el que leeremos el nivel de luminosidad
+
+  Serial.begin(115200);  
+  while (!Serial); 
+
+  Serial.println("LoRa Duplex with TxDone and Receive callbacks");
+  Serial.println("Using binary packets");
+  
+  // Es posible indicar los pines para CS, reset e IRQ pins (opcional)
+  // LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  
+  if (!init_PMIC()) {
+    Serial.println("Initilization of BQ24195L failed!");
+  }
+  else {
+    Serial.println("Initilization of BQ24195L succeeded!");
+  }
+
+  if (!LoRa.begin(868E6)) {      // Initicializa LoRa a 868 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                
+  }
+
+  // Configuramos algunos parámetros de la radio
+  LoRa.setSignalBandwidth(long(bandwidth_kHz[thisNodeConf.bandwidth_index])); 
+                                  // 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3
+                                  // 41.7E3, 62.5E3, 125E3, 250E3, 500E3 
+                                  // Multiplicar por dos el ancho de banda
+                                  // supone dividir a la mitad el tiempo de Tx
+                                  
+  LoRa.setSpreadingFactor(thisNodeConf.spreadingFactor);     
+                                  // [6, 12] Aumentar el spreading factor incrementa 
+                                  // de forma significativa el tiempo de Tx
+                                  // SPF = 6 es un valor especial
+                                  // Ver tabla 12 del manual del SEMTECH SX1276
+  
+  LoRa.setCodingRate4(thisNodeConf.codingRate);         
+                                  // [5, 8] 5 da un tiempo de Tx menor
+                                  
+  LoRa.setTxPower(thisNodeConf.txPower, PA_OUTPUT_PA_BOOST_PIN); 
+                                  // Rango [2, 20] en dBm
+                                  // Importante seleccionar un valor bajo para pruebas
+                                  // a corta distancia y evitar saturar al receptor
+  LoRa.setSyncWord(0x12);         // Palabra de sincronización privada por defecto para SX127X 
+                                  // Usaremos la palabra de sincronización para crear diferentes
+                                  // redes privadas por equipos
+  LoRa.setPreambleLength(8);      // Número de símbolos a usar como preámbulo
+
+  
+  // Indicamos el callback para cuando se reciba un paquete
+  LoRa.onReceive(onReceive);
+  
+  // Activamos el callback que nos indicará cuando ha finalizado la 
+  // transmisión de un mensaje
+  LoRa.onTxDone(TxFinished);
+
+  // Nótese que la recepción está activada a partir de este punto
+  LoRa.receive();
+
+  Serial.println("LoRa init succeeded.\n");
 }
 
 void loop() 
 {
-  float raw = analogRead(SensorPIN);
+  static uint32_t lastSendTime_ms = 0;
+  static uint32_t txInterval_ms = TX_LAPSE_MS;
+  static uint32_t tx_begin_ms = 0;
+
+  uint8_t payload[2];
+  uint8_t payloadLength = 2;
+
+  d_read = -1;
+  a_read = -1;
+  if(!transmitting) {
+    temperatureMeasure();
+  }
+  
+}
+
+void temperatureMeasure() {
+ float raw = analogRead(SensorPIN);
   float V =  raw / 1024 * Vcc;
 
   float R = (Rc * V ) / (Vcc - V);
@@ -38,5 +149,6 @@ void loop()
   Serial.print("T = ");
   Serial.print(celsius);
   Serial.print("C\n");
-  delay(2500);
+  delay(2500); 
+  
 }
